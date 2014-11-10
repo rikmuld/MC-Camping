1708d732a3793994e617f449e401aba7c71bd64c
RikMuld <rikmuld@gmail.com>
2014-11-10 23:02:02 +0100

    Some work on campers.

--- Source/com/rikmuld/camping/core/Library.scala
+++ Source/com/rikmuld/camping/core/Library.scala
@@ -4,7 +4,7 @@ import org.lwjgl.input.Keyboard
 
 object ModInfo {
   final val MOD_ID = "camping"
-  final val MOD_NAME = "The Camping Mod 2.0"
+  final val MOD_NAME = "The Camping Mod 2"
   final val MOD_VERSION = "2.1"
   final val MOD_LANUAGE = "scala"
   final val MOD_DEPENDENCIES = "required-after:Forge@[v10.13.2.1231,)"
